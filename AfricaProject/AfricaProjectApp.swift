1a51379f9305ed19f412e0af938a47d94edcb1d4
Orlando Moraes Martins <orlandomoraesmartins@gmail.com>
2023-01-28 18:03:21 -0300

    Initial Commit

---
+++ AfricaProject/AfricaProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaProjectApp.swift
+//  AfricaProject
+//
+//  Created by Orlando Moraes Martins on 28/01/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
